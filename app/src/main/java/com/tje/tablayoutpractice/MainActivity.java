6724c0d208c427fb818b70cc24d314bc1901b58a
unnato0159 <unnato01@naver.com>
2019-05-18 11:18:57 +0900

    커스텀 탭 뷰 마무리

--- app/src/main/java/com/tje/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/tje/tablayoutpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.tje.tablayoutpractice;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.tje.tablayoutpractice.databinding.ActivityMainBinding;
 
@@ -30,13 +33,22 @@ public class MainActivity extends BaseActivity {
         act.tablayout.addTab(act.tablayout.newTab().setText("홈").setIcon(R.mipmap.ic_launcher));
         act.tablayout.addTab(act.tablayout.newTab().setText("채팅목록"));
         act.tablayout.addTab(act.tablayout.newTab().setText("검색"));
-        act.tablayout.addTab(act.tablayout.newTab().setText("더보기"));
-        act.tablayout.addTab(act.tablayout.newTab().setText("더보기"));
-        act.tablayout.addTab(act.tablayout.newTab().setText("더보기"));
-        act.tablayout.addTab(act.tablayout.newTab().setText("더보기"));
-        act.tablayout.addTab(act.tablayout.newTab().setText("더보기"));
+        act.tablayout.addTab(act.tablayout.newTab().setCustomView(createCoutomTabView("커스텀")));
+        act.tablayout.addTab(act.tablayout.newTab().setCustomView(createCoutomTabView("커스텀2")));
+
 
     }
+
+    View createCoutomTabView(String tabName){
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab,null);
+
+        TextView nameTxt = tabView.findViewById(R.id.nameTxt);
+        nameTxt.setText(tabName);
+
+        return tabView;
+
+    }
+
 
     @Override
     public void bindViews() {
